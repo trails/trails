1634bbe549ce6300de59819f4b99819294f435c4
Grayson Stebbins <grayson@Graysons-MacBook-Pro.local>
2013-03-20 20:57:05 -0700

    Upgrade to rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheApp::Application.config.secret_token = '2f63b146c6a7c82e64d6fbcb869af12c6f5a1bd8bd84a26751e4630d803c979466c3a4a3fa265284e95cb284df886cc75069fc948afc7ef70d212089fac75441'
